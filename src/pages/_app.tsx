04c61bb34476a61367eb498d02b72ce36687bb22
hamid <hamidlahiane2005@gmail.com>
2026-01-17 23:32:24 +0100

    mise a jour

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script' // Importez Script pour la performance
 
@@ -6,11 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* Script Umami */}
-      <Script 
-        async 
-        src="https://cloud.umami.is/script.js" 
-        data-website-id="e7b53df7-ac74-49a4-b3d6-1ab2c74770d6" 
-      />
+      <script defer src="https://cloud.umami.is/script.js" data-website-id="e7b53df7-ac74-49a4-b3d6-1ab2c74770d6"></script>
       
       <Component {...pageProps} />
     </>
