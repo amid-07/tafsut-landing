e2484a32556a2e678f830d7a75e6b44eda5d9565
hamid <hamidlahiane2005@gmail.com>
2026-01-17 23:26:28 +0100

    mise a jour

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* Script Umami */}
-      <script defer src="https://cloud.umami.is/script.js" data-website-id="e7b53df7-ac74-49a4-b3d6-1ab2c74770d6"></script>
+      <Script 
+        async 
+        src="https://cloud.umami.is/script.js" 
+        data-website-id="e7b53df7-ac74-49a4-b3d6-1ab2c74770d6" 
+      />
       
       <Component {...pageProps} />
     </>
